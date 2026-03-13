cb60c5c9450cbb0a7e4594fe5eafcb1f2814ed87
unknown <lyin@noon.com>
2026-03-13 11:31:07 +0400

    add exf2l cases

--- three.py
+++ three.py
@@ -655,6 +655,12 @@ def main(allowed_moves='RUF'):
     init_cube = Cube(''.join(['nnnn', 'nngn', 'nngw', 'nnoo', 'nnnb', 'nrww']),
                      ''.join(['nnnn', 'nnrg', 'nggn', 'nooo', 'nrbb', 'wwww']),
                      'yrgobw')
+    init_cube = Cube(''.join(['nnnn', 'nngn', 'nngw', 'nnoo', 'nnnb', 'nrww']),
+                     ''.join(['nnnn', 'nnrr', 'nggn', 'nooo', 'ngbb', 'wwww']),
+                     'yrgobw')
+    init_cube = Cube(''.join(['nnnn', 'nnng', 'nngn', 'nnoo', 'nnrb', 'wnww']),
+                     ''.join(['nnnn', 'ngrn', 'nggr', 'nooo', 'nnbb', 'wwww']),
+                     'yrgobw')
     target_cube = Cube(''.join(['nnnn', 'nnnn', 'nngn', 'nnoo', 'nnnb', 'nnww']),
                        ''.join(['nnnn', 'nnrn', 'nggn', 'nooo', 'nnbb', 'wwww']),
                        'yrgobw')
